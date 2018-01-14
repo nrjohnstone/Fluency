cc44d8785feeca13181ec30d6ef40a73af577e59
nrjohnstone <locotewz@gmail.com>
2018-01-14 16:23:13 +0100

    [implement] add generated versioning file from BeforeBuild task

---
+++ src/AssemblyVersionInfo.template.cs
@@ -0,0 +1,7 @@
+using System.Reflection;
+
+// GENERATED FILE - DO NOT EDIT
+
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0-initialbuild")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
